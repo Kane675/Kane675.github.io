3f46b5a980b920b5824218ae548eb81b79a6ce33
Kane Piggott <91965676+Kane675@users.noreply.github.com>
2025-07-31 00:30:21 +0100

    Almost there

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+    output: 'export', // Enables static export
+    images: {
+        unoptimized: true // Required for static image export
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

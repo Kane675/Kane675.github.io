9a2e1094b47d04c6d72d8b909f001ca7bd12f78b
Kane Piggott <91965676+Kane675@users.noreply.github.com>
2025-07-31 19:04:18 +0100

    config twweaks

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    //output: 'export', // Enables static export
-    //basePath: '', 
     images: {
-        unoptimized: true // Required for static image export
+        unoptimized: true 
     },
 };
 

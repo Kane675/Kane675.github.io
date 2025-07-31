8c6b7b248f024460496b5c14264f2c5ba98ee7f2
Kane Piggott <91965676+Kane675@users.noreply.github.com>
2025-07-31 01:29:17 +0100

    Restoring from backup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
     output: 'export', // Enables static export
+    basePath: '', 
     images: {
         unoptimized: true // Required for static image export
     },
